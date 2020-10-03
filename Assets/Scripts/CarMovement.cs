37e381c979834104048c29099a2b7348b07b1b6a
Miguel Fernandez <miguelrf95@gmail.com>
2020-10-03 12:47:34 +0100

    Create debug material and intial move script

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float _maxSpeed;
+    private Rigidbody _rb;
+    private Vector3 _moveVector;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _moveVector = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Method that is comprised of the acceleration methods
+    /// </summary>
+    private void Movement()
+    {
+        if (Input.GetButton("Accelerator"))
+        {
+            Accelerate();
+        }
+        else
+        {
+            Decelerate();
+        }
+    }
+
+    /// <summary>
+    /// Method to accelerate the car
+    /// </summary>
+    private void Accelerate()
+    {
+ 
+    }
+
+    /// <summary>
+    /// Method to decelerate the car
+    /// </summary>
+    private void Decelerate()
+    {
+
+    }
+}
