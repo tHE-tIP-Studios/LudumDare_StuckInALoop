43cfc8966d1700806bfc15da67881ae86c864086
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2020-10-03 18:43:19 +0100

    Car prefab and smoke particles

---
+++ Assets/Scripts/PlayerScripts/CarEffects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEffects : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _startSmoke;
+
+    public void StartMovementSmoke()
+    {
+        _startSmoke.Play();
+    }
+}
